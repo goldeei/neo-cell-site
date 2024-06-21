9ee350177c047921b45280d01955d36fa264602b
Jake <jake.goldiee@gmail.com>
2024-06-20 22:03:11 -0400

    Install tw utils

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,45 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				"dark-blue": {
+					500: "#1F345A",
+				},
+				"mid-grey": {
+					500: "#708090",
+				},
+				"dark-grey": {
+					500: "#36454F",
+				},
+				"river-blue": {
+					500: "#66C7EA",
+				},
+				"sky-blue": {
+					500: "#E0F0FF",
+				},
+			},
+			backgroundImage: {
+				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+			},
+			backdropFilter: {
+				none: "none",
+				blur: "blur(10px)",
+				brightness: "brightness(1.5)",
+			},
+		},
+	},
+	variants: {
+		extend: {
+			backdropFilter: ["responsive", "hover", "focus"],
+		},
+	},
+	plugins: [require("tailwindcss-filters")],
 };
 export default config;
