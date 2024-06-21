bec63d30d9b2eb40123e72ed2ddd9c5d07cf46fa
Jake <jake.goldiee@gmail.com>
2024-06-21 02:09:32 -0400

    Add main layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import { LandingPage } from './landing-page';
+
 export default function Home() {
-	return <div>Main</div>;
+	return (
+		<div className="h-svh w-svh flex justify-center">
+			<LandingPage />
+		</div>
+	);
 }
