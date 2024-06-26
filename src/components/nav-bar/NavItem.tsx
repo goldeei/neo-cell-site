51b9005e12be15965dee654a2365fb021dcf6001
Jake <jake.goldiee@gmail.com>
2024-06-25 23:56:38 -0400

    Add active link handling

--- src/components/nav-bar/NavItem.tsx
+++ src/components/nav-bar/NavItem.tsx
@@ -1,11 +1,27 @@
+import cn from '@/utilities/cn';
+
 interface NavItemProps {
 	link: string;
 	label: string;
+	onSetLink: (link: string) => void;
+	isActive: boolean;
 }
-export const NavItem = ({ link, label }: NavItemProps) => {
+export const NavItem = ({ link, label, onSetLink, isActive }: NavItemProps) => {
+	const handleClick = () => {
+		onSetLink(link);
+	};
 	return (
 		<li>
-			<a href={link}>{label}</a>
+			<a
+				href={link}
+				onClick={handleClick}
+				className={cn(
+					"text-header-blue-500 font-medium",
+					isActive && "text-blue-500"
+				)}
+			>
+				{label}
+			</a>
 		</li>
 	);
 };
--- src/components/nav-bar/index.tsx
+++ src/components/nav-bar/index.tsx
@@ -1,14 +1,57 @@
+"use client";
+
+import { motion } from 'framer-motion';
+import { useEffect, useState } from 'react';
+
 import { NavItem } from './NavItem';
 
 export const NavBar = () => {
+	const [activeLink, setActiveLink] = useState("");
+
+	useEffect(() => {
+		console.log(activeLink);
+	}, [activeLink]);
+
 	return (
 		<nav className="sticky top-0 z-50 shadow-lg border border-white w-full p-2 bg-gradient-to-b from-white/80 to-white/60 backdrop-blur-[6px]">
 			<div className="max-w-[1920px] h-14 mx-auto flex justify-between items-center">
-				<img src={"/NeoCellLogo.png"} className="h-full w-fit" />
-				<ul className="flex justify-between gap-8">
-					<NavItem label="Technology" link="#technology" />
-					<NavItem label="About" link="#about" />
-					<NavItem label="Meet The Team" link="#meet-the-team" />
+				<a
+					href="#landing-page"
+					className="h-full w-fit"
+					onClick={() => setActiveLink("")}
+				>
+					<img src={"/NeoCellLogo.png"} className="h-full w-full" />
+				</a>
+				<ul className="flex justify-between items-center gap-8">
+					<NavItem
+						onSetLink={setActiveLink}
+						isActive={activeLink === "#technology"}
+						label="Technology"
+						link="#technology"
+					/>
+					<NavItem
+						onSetLink={setActiveLink}
+						isActive={activeLink === "#about"}
+						label="About"
+						link="#about"
+					/>
+					<NavItem
+						onSetLink={setActiveLink}
+						isActive={activeLink === "#meet-the-team"}
+						label="Meet The Team"
+						link="#meet-the-team"
+					/>
+					<motion.a
+						aria-label="Send mail"
+						href="mailto: mail@mail.com"
+						className="border bg-header-blue-500 px-8 py-2 rounded-full transition-colors ease-in-out hover:bg-blue-500 text-white shadow-none hover:shadow-md"
+						whileHover={{ y: -2 }}
+						transition={{
+							duration: 0.15,
+						}}
+					>
+						Contact Us
+					</motion.a>
 				</ul>
 			</div>
 		</nav>
