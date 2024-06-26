5342cd94f3d3e50a2b9028de647201ebbfbb8060
Jake <jake.goldiee@gmail.com>
2024-06-25 22:55:54 -0400

    Add Navbar

---
+++ src/components/nav-bar/index.tsx
@@ -0,0 +1,9 @@
+export const NavBar = () => {
+	return (
+		<div className="sticky top-0 z-50 shadow-lg border border-white w-full p-2 bg-gradient-to-b from-white/80 to-white/60 backdrop-blur-[6px]">
+			<div className="max-w-[1920px] h-14 mx-auto">
+				<img src={"/NeoCellLogo.png"} className="h-full w-fit" />
+			</div>
+		</div>
+	);
+};
