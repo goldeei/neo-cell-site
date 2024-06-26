628d2a47155139b0daa63abb844801cb537924f4
Jake <jake.goldiee@gmail.com>
2024-06-25 22:55:38 -0400

    Video to own fixed component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
+import { LandingVid } from '@/components/landing-vid';
+import { NavBar } from '@/components/nav-bar';
+
 import { About } from './about';
 import { LandingPage } from './landing-page';
 import { Technology } from './technology';
@@ -5,6 +8,8 @@ import { Technology } from './technology';
 export default function Home() {
 	return (
 		<div className="">
+			<NavBar />
+			<LandingVid />
 			<LandingPage />
 			<Technology />
 			<About />
--- src/components/landing-vid/index.tsx
+++ src/components/landing-vid/index.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useRef, useState } from 'react';
 import { useWindowSize } from 'usehooks-ts';
 
-export const LandingPage = () => {
+export const LandingVid = () => {
 	const { width = 0 } = useWindowSize();
 	const [translateX, setTranslateX] = useState(0);
 
@@ -19,14 +19,20 @@ export const LandingPage = () => {
 	}, [width]);
 
 	return (
-		<section className="landing-page relative flex flex-col justify-center">
-			<div className="text-center lg:text-[3svw] w-full px-6 font-medium tracking-tighter leading-tight">
-				Unlocking Clinical Support <br></br>using PET-enabled therapies
-			</div>
-			<div className="text-xl text-center lg:text-[2svw] leading-tight">
-				Access new data to monitor patients and <br></br>evaluate cell and gene
-				therapies
-			</div>
-		</section>
+		<div className="fixed top-0 left-0 landing-page flex justify-center h-svh w-svw -z-50">
+			<video
+				className={`h-full w-full min-w-[1360px] object-cover z-0 brightness-110 saturate-[1.3] `}
+				autoPlay
+				loop
+				muted
+				preload="auto"
+				style={{
+					transform: `translateX(${translateX}px)`,
+				}}
+				ref={videoRef}
+			>
+				<source src="/NeoCellLandingPage.mp4" type="video/mp4" />
+			</video>
+		</div>
 	);
 };
