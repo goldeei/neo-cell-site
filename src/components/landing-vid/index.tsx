b2ca4e44f31423cc5239af910b5f1e3715533dae
Jake <jake.goldiee@gmail.com>
2024-06-25 23:23:16 -0400

    Hide video and unmount when passed landing page

--- src/components/landing-vid/index.tsx
+++ src/components/landing-vid/index.tsx
@@ -1,13 +1,21 @@
 "use client";
 
+import { motion, useMotionValueEvent, useScroll } from 'framer-motion';
 import { useEffect, useRef, useState } from 'react';
 import { useWindowSize } from 'usehooks-ts';
 
 export const LandingVid = () => {
+	const { scrollYProgress } = useScroll();
+	const [coverOpacity, setCoverOpacity] = useState(0);
+
 	const { width = 0 } = useWindowSize();
 	const [translateX, setTranslateX] = useState(0);
 
 	const videoRef = useRef<HTMLVideoElement>(null);
+
+	useMotionValueEvent(scrollYProgress, "change", (latest) => {
+		setCoverOpacity(latest * 2 + 0.1);
+	});
 
 	useEffect(() => {
 		if (width < 1360 && videoRef.current) {
@@ -20,19 +28,28 @@ export const LandingVid = () => {
 
 	return (
 		<div className="fixed top-0 left-0 landing-page flex justify-center h-svh w-svw -z-50">
-			<video
-				className={`h-full w-full min-w-[1360px] object-cover z-0 brightness-110 saturate-[1.3] `}
-				autoPlay
-				loop
-				muted
-				preload="auto"
-				style={{
-					transform: `translateX(${translateX}px)`,
-				}}
-				ref={videoRef}
-			>
-				<source src="/NeoCellLandingPage.mp4" type="video/mp4" />
-			</video>
+			<div
+				className="absolute top-0 left-0 h-full w-full bg-white backdrop-blur-sm"
+				style={{ opacity: coverOpacity }}
+			/>
+			{coverOpacity < 1 && (
+				<motion.video
+					initial={{ opacity: 0 }}
+					animate={{ opacity: 1 }}
+					exit={{ opacity: 0 }}
+					className={`h-full w-full min-w-[1360px] object-cover -z-10 brightness-110 saturate-[1.3] `}
+					autoPlay
+					loop
+					muted
+					preload="auto"
+					style={{
+						transform: `translateX(${translateX}px)`,
+					}}
+					ref={videoRef}
+				>
+					<source src="/NeoCellLandingPage.mp4" type="video/mp4" />
+				</motion.video>
+			)}
 		</div>
 	);
 };
