09ed315f971551fbf3196cb8424d53bbb95c05ba
Jake <jake.goldiee@gmail.com>
2024-06-26 00:50:02 -0400

    Remove test layout

--- src/app/technology/page.tsx
+++ src/app/technology/page.tsx
@@ -25,25 +25,11 @@ const Hexagon = ({
 
 export const Technology = () => {
 	return (
-		<motion.section id="technology" className="page-gradient flex items-center">
-			<div className="max-w-svw flex items-center overflow-hidden">
-				<div className="flex-1 flex flex-col gap-12">
-					<div className="text-6xl text-header-blue-500">TECHNOLOGY</div>
-					<div className="text-3xl">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod
-						ratione quos numquam impedit non quis. Assumenda molestias
-						doloremque officiis dolores quasi nam itaque quae perspiciatis
-						consectetur deleniti tempore, quam neque. Lorem ipsum dolor sit amet
-						consectetur adipisicing elit. Quod ratione quos numquam impedit non
-						quis. Assumenda molestias doloremque officiis dolores quasi nam
-						itaque quae perspiciatis consectetur deleniti tempore, quam neque.
-					</div>
-				</div>
-
-				<div className="flex-1 flex justify-center">
-					<SvgHexCells />
-				</div>
-			</div>
+		<motion.section
+			id="technology"
+			className="page-gradient flex  justify-around"
+		>
+			<div className="max-w-svw flex flex-col  justify-around"></div>
 		</motion.section>
 	);
 };
