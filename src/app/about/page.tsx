93492c878afd7731125f24b661515d0cdda97a88
Jake <jake.goldiee@gmail.com>
2024-06-29 14:12:55 -0400

    Refine responsive layout

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,16 +1,16 @@
-import { HexagonInfo } from '@/components/HexagonInfo';
-import BeakerIcon from '@/components/svg-components/Beaker';
+import { HexagonInfo } from "@/components/HexagonInfo";
+import { Section } from "@/components/section";
+import BeakerIcon from "@/components/svg-components/Beaker";
 
 const About = () => {
 	return (
-		<section id="about" className="page-gradient">
-			{/* <div className="flex flex-col md:flex-row items-center justify-around gap-24"> */}
-			<div className="flex flex-col justify-between lg:grid gap-auto lg:grid-cols-6 lg:grid-rows-1 items-center">
-				<div className="gap-6 lg:col-span-4 flex flex-col lg:gap-12">
-					<div className="text-header-blue-500 text-3xl md:text-6xl leading-tight font-medium tracking-tight">
+		<Section id="about" className="page-gradient">
+			<div className="h-full flex flex-col justify-between items-around lg:grid gap-auto lg:grid-cols-6 lg:grid-rows-1">
+				<div className="flex-1 gap-6 lg:col-span-4 flex flex-col justify-center lg:gap-12">
+					<div className="text-header-blue-500 text-3xl md:text-4xl xl:text-6xl leading-tight font-medium tracking-tight">
 						Revolutionizing Cell Therapy Monitoring & Analysis
 					</div>
-					<div className="text-xl md:text-2xl flex flex-col gap-6 font-light leading-snug">
+					<div className="text-lg sm:text-xl xl:text-2xl 2xl:text-3xl flex flex-col gap-6 font-light leading-snug">
 						<div>
 							By leveraging PET reporter technology, our innovative approach
 							enables early detection of low efficacy or high toxicity
@@ -27,19 +27,21 @@ const About = () => {
 						</div>
 					</div>
 				</div>
-				<div className="h-full items-center lg:h-fit grid gap-4 grid-cols-3 lg:grid-cols-1 lg:grid-rows-3 lg:col-start-6">
-					<div className=" lg:-translate-x-1/2 lg:translate-y-1/4">
-						<HexagonInfo icon={<BeakerIcon />} />
-					</div>
-					<div>
-						<HexagonInfo icon={<BeakerIcon />} />
-					</div>
-					<div className=" lg:-translate-x-1/2 lg:-translate-y-1/4">
-						<HexagonInfo icon={<BeakerIcon />} />
+				<div className="col-span-2 flex items-center justify-end">
+					<div className="w-1/2 items-center grid gap-4 grid-cols-3 lg:grid-cols-1 lg:grid-rows-3 lg:col-start-6">
+						<div className=" lg:-translate-x-1/2 lg:translate-y-1/4">
+							<HexagonInfo icon={<BeakerIcon />} />
+						</div>
+						<div>
+							<HexagonInfo icon={<BeakerIcon />} />
+						</div>
+						<div className=" lg:-translate-x-1/2 lg:-translate-y-1/4">
+							<HexagonInfo icon={<BeakerIcon />} />
+						</div>
 					</div>
 				</div>
 			</div>
-		</section>
+		</Section>
 	);
 };
 
