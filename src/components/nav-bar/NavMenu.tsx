a83d5c1c3b5e25cd377dd31277c3064a9a0b7c98
Jake <jake.goldiee@gmail.com>
2024-06-29 14:18:58 -0400

    Extract navmenu, fix full width nav hiding on click

---
+++ src/components/nav-bar/NavMenu.tsx
@@ -0,0 +1,117 @@
+import cn from "@/utilities/cn";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { AnimatePresence, motion } from "framer-motion";
+import { useEffect, useRef, useState } from "react";
+import { useOnClickOutside, useWindowSize } from "usehooks-ts";
+
+import { NavItem } from "./NavItem";
+import { SectionId } from "./types";
+
+const NAV_MENU_CLASSES = {
+	default: "justify-between items-center",
+	collapsed: cn(
+		"w-full sm:w-72 flex-1 bg-white fixed right-0 top-full flex-col gap-12 rounded justify-between px-8 py-8 mt-1 me-0 sm:me-1 shadow-lg"
+	),
+};
+
+interface NavMenuProps {
+	setActiveLink: (linkName: SectionId) => void;
+	activeLink: SectionId;
+}
+export const NavMenu = ({ setActiveLink, activeLink }: NavMenuProps) => {
+	const [isCollapsed, setIsCollapsed] = useState(false);
+	const [isVisible, setIsVisible] = useState(false);
+	const handleSetVisible = () => {
+		setIsVisible(!isVisible);
+	};
+
+	const handleClickOutside = () => {
+		if (isVisible) {
+			setIsVisible(false);
+		}
+	};
+
+	const ref = useRef<HTMLDivElement>(null);
+	useOnClickOutside(ref, handleClickOutside);
+
+	const { width } = useWindowSize();
+	useEffect(() => {
+		if (width <= 768) {
+			setIsCollapsed(true);
+			setIsVisible(false);
+		} else {
+			setIsCollapsed(false);
+			setIsVisible(true);
+		}
+	}, [width]);
+	return (
+		<div ref={ref}>
+			{/* TODO hamburger appears in weird spot, then adjust position */}
+			{isCollapsed && (
+				<button
+					onMouseDown={handleSetVisible}
+					className="flex justify-center w-6 h-6"
+				>
+					<FontAwesomeIcon
+						className={cn(
+							"w-full h-full",
+							isVisible ? "text-blue-500" : "text-header-blue-500"
+						)}
+						icon={faBars}
+					/>
+				</button>
+			)}
+			<AnimatePresence>
+				{isVisible || !isCollapsed ? (
+					<motion.ul
+						className={cn(
+							"flex gap-8",
+							isCollapsed
+								? NAV_MENU_CLASSES.collapsed
+								: NAV_MENU_CLASSES.default
+						)}
+						initial={{ opacity: 0 }}
+						animate={{ opacity: 1 }}
+						exit={{ opacity: 0 }}
+					>
+						<NavItem
+							onSetLink={setActiveLink}
+							isActive={activeLink === "#about"}
+							label="About"
+							link="#about"
+							isCollapsed={isCollapsed}
+						/>
+						<NavItem
+							onSetLink={setActiveLink}
+							isActive={activeLink === "#technology"}
+							label="Technology"
+							link="#technology"
+							isCollapsed={isCollapsed}
+						/>
+						<NavItem
+							onSetLink={setActiveLink}
+							isActive={activeLink === "#meet-the-team"}
+							label="Meet The Team"
+							link="#meet-the-team"
+							isCollapsed={isCollapsed}
+						/>
+						<li>
+							<motion.a
+								aria-label="Send mail"
+								href="mailto: mail@mail.com"
+								className="border bg-header-blue-500 px-8 py-2 rounded-full transition-colors ease-in-out hover:bg-blue-500 text-white shadows hover:shadow-md whitespace-nowrap"
+								whileHover={{ y: -2 }}
+								transition={{
+									duration: 0.15,
+								}}
+							>
+								Contact Us
+							</motion.a>
+						</li>
+					</motion.ul>
+				) : null}
+			</AnimatePresence>
+		</div>
+	);
+};
