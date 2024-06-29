16becc8e3c1ae87961f18ae2666f49c5e4368544
Jake <jake.goldiee@gmail.com>
2024-06-29 14:13:05 -0400

    Position within a flex parent

--- src/components/HexagonInfo.tsx
+++ src/components/HexagonInfo.tsx
@@ -4,10 +4,16 @@ interface HexagonInfoProps {
 
 export const HexagonInfo = ({ icon }: HexagonInfoProps) => {
 	return (
-		<div className="relative">
-			<img src="/Hexagon.png" alt="" className="w-full h-auto aspect-auto" />
-			<div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-sky-50">
-				{icon}
+		<div className="flex justify-end items-center">
+			<div className="relative">
+				<img
+					src="/Hexagon.png"
+					alt=""
+					className="w-full max-w-60 h-auto aspect-auto"
+				/>
+				<div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-sky-50">
+					{icon}
+				</div>
 			</div>
 		</div>
 	);
