5e1277d036e5551a3a779b41c84d0310b4d6fbdd
Jake <jake.goldiee@gmail.com>
2024-06-29 12:32:16 -0400

    Add collapsible nav bar

--- src/components/nav-bar/NavItem.tsx
+++ src/components/nav-bar/NavItem.tsx
@@ -1,17 +1,26 @@
 import cn from '@/utilities/cn';
 
+import { SectionId } from './types';
+
 interface NavItemProps {
-	link: string;
+	link: SectionId;
 	label: string;
-	onSetLink: (link: string) => void;
+	onSetLink: (link: SectionId) => void;
 	isActive: boolean;
+	isCollapsed: boolean;
 }
-export const NavItem = ({ link, label, onSetLink, isActive }: NavItemProps) => {
+export const NavItem = ({
+	link,
+	label,
+	onSetLink,
+	isActive,
+	isCollapsed,
+}: NavItemProps) => {
 	const handleClick = () => {
 		onSetLink(link);
 	};
 	return (
-		<li>
+		<li className="relative">
 			<a
 				href={link}
 				onClick={handleClick}
@@ -22,6 +31,15 @@ export const NavItem = ({ link, label, onSetLink, isActive }: NavItemProps) => {
 			>
 				{label}
 			</a>
+			{isActive && (
+				<>
+					{isCollapsed ? (
+						<div className="absolute w-2 h-2 bg-blue-500 rounded-full left-0 top-1/2 -translate-y-1/2" />
+					) : (
+						<div className="absolute w-[calc(100%+0.5rem)] h-1 bg-blue-500 rounded-md left-1/2 -translate-x-1/2" />
+					)}
+				</>
+			)}
 		</li>
 	);
 };
--- src/components/nav-bar/index.tsx
+++ src/components/nav-bar/index.tsx
@@ -1,19 +1,129 @@
 "use client";
 
-import { motion } from 'framer-motion';
+import cn from '@/utilities/cn';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { AnimatePresence, motion } from 'framer-motion';
 import { useEffect, useState } from 'react';
 import { useWindowSize } from 'usehooks-ts';
 
 import { NavItem } from './NavItem';
+import { SectionId } from './types';
+
+const BACKGROUND_CLASSES =
+	"border-white bg-gradient-to-b from-white/80 to-white/60 backdrop-blur-sm";
+const NAV_MENU_CLASSES = {
+	default: "justify-between items-center",
+	collapsed: cn(
+		"w-full sm:w-72 flex-1 bg-white fixed right-0 top-full flex-col gap-12 rounded justify-between px-8 py-8 mt-1 me-0 sm:me-1 shadow-lg"
+	),
+};
+interface NavMenuProps {
+	setActiveLink: (linkName: SectionId) => void;
+	activeLink: SectionId;
+}
+const NavMenu = ({ setActiveLink, activeLink }: NavMenuProps) => {
+	const [isCollapsed, setIsCollapsed] = useState(false);
+	const [isVisible, setIsVisible] = useState(false);
+	const handleSetVisible = () => {
+		setIsVisible(!isVisible);
+	};
+
+	const { width } = useWindowSize();
+	useEffect(() => {
+		if (width <= 768) {
+			setIsCollapsed(true);
+			setIsVisible(false);
+		} else {
+			setIsCollapsed(false);
+			setIsVisible(true);
+		}
+	}, [width]);
+	return (
+		<>
+			{isCollapsed && (
+				<button
+					onMouseDown={handleSetVisible}
+					className="flex justify-center w-6 h-6"
+				>
+					<FontAwesomeIcon
+						className={cn(
+							"w-full h-full",
+							isVisible ? "text-blue-500" : "text-header-blue-500"
+						)}
+						icon={faBars}
+					/>
+				</button>
+			)}
+			<AnimatePresence>
+				{isVisible ? (
+					<motion.ul
+						className={cn(
+							"flex gap-8",
+							isCollapsed
+								? NAV_MENU_CLASSES.collapsed
+								: NAV_MENU_CLASSES.default
+						)}
+						initial={{ opacity: 0 }}
+						animate={{ opacity: 1 }}
+						exit={{ opacity: 0 }}
+					>
+						<NavItem
+							onSetLink={setActiveLink}
+							isActive={activeLink === "#about"}
+							label="About"
+							link="#about"
+							isCollapsed={isCollapsed}
+						/>
+						<NavItem
+							onSetLink={setActiveLink}
+							isActive={activeLink === "#technology"}
+							label="Technology"
+							link="#technology"
+							isCollapsed={isCollapsed}
+						/>
+						<NavItem
+							onSetLink={setActiveLink}
+							isActive={activeLink === "#meet-the-team"}
+							label="Meet The Team"
+							link="#meet-the-team"
+							isCollapsed={isCollapsed}
+						/>
+						<li>
+							<motion.a
+								aria-label="Send mail"
+								href="mailto: mail@mail.com"
+								className="border bg-header-blue-500 px-8 py-2 rounded-full transition-colors ease-in-out hover:bg-blue-500 text-white shadows hover:shadow-md whitespace-nowrap"
+								whileHover={{ y: -2 }}
+								transition={{
+									duration: 0.15,
+								}}
+							>
+								Contact Us
+							</motion.a>
+						</li>
+					</motion.ul>
+				) : null}
+			</AnimatePresence>
+		</>
+	);
+};
 
 export const NavBar = () => {
-	const [activeLink, setActiveLink] = useState("");
+	const [activeLink, setActiveLink] = useState<SectionId>("");
 
-	const { width = 0 } = useWindowSize();
+	const handleActiveLinkChange = (linkName: SectionId) => {
+		setActiveLink(linkName);
+	};
 
 	return (
-		<nav className="sticky top-0 z-50 shadow-lg border border-white w-full px-4 py-2 bg-gradient-to-b from-white/80 to-white/60 backdrop-blur-[6px]">
-			<div className="page-width h-14 mx-auto flex justify-between items-center">
+		<nav
+			className={cn(
+				"sticky top-0 z-50 shadow-lg border w-full px-4 py-2",
+				BACKGROUND_CLASSES
+			)}
+		>
+			<div className="relative page-width h-14 mx-auto flex justify-between items-center">
 				<a
 					href="#landing-page"
 					className="h-full w-fit"
@@ -21,39 +131,10 @@ export const NavBar = () => {
 				>
 					<img src={"/NeoCellLogo.png"} className="h-full w-full" />
 				</a>
-				<ul className="flex justify-between items-center gap-8">
-					<NavItem
-						onSetLink={setActiveLink}
-						isActive={activeLink === "#about"}
-						label="About"
-						link="#about"
-					/>
-					<NavItem
-						onSetLink={setActiveLink}
-						isActive={activeLink === "#technology"}
-						label="Technology"
-						link="#technology"
-					/>
-					<NavItem
-						onSetLink={setActiveLink}
-						isActive={activeLink === "#meet-the-team"}
-						label="Meet The Team"
-						link="#meet-the-team"
-					/>
-					<li>
-						<motion.a
-							aria-label="Send mail"
-							href="mailto: mail@mail.com"
-							className="border bg-header-blue-500 px-8 py-2 rounded-full transition-colors ease-in-out hover:bg-blue-500 text-white shadow-none hover:shadow-md"
-							whileHover={{ y: -2 }}
-							transition={{
-								duration: 0.15,
-							}}
-						>
-							Contact Us
-						</motion.a>
-					</li>
-				</ul>
+				<NavMenu
+					setActiveLink={handleActiveLinkChange}
+					activeLink={activeLink}
+				/>
 			</div>
 		</nav>
 	);
