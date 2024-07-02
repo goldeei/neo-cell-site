750245dd7ed26746fac6847c6d0ef9d8a08c24e6
Jake <jake.goldiee@gmail.com>
2024-07-01 22:17:04 -0400

    Refine layout

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,12 +5,12 @@ import BeakerIcon from "@/components/svg-components/Beaker";
 const About = () => {
 	return (
 		<Section id="about" className="page-gradient">
-			<div className="h-full flex flex-col justify-between items-around lg:grid gap-auto lg:grid-cols-6 lg:grid-rows-1">
-				<div className="flex-1 gap-6 lg:col-span-4 flex flex-col justify-center lg:gap-12">
-					<div className="text-header-blue-500 text-3xl md:text-4xl xl:text-6xl leading-tight font-medium tracking-tight">
+			<div className="h-full gap-20 flex flex-col justify-center lg:justify-between lg:items-around lg:grid lg:gap-auto lg:grid-cols-6 lg:grid-rows-1">
+				<div className="gap-6 lg:col-span-4 flex flex-col justify-center lg:gap-12">
+					<div className="text-header-blue-500 text-3xl md:text-5xl xl:text-6xl leading-tight font-medium tracking-tight">
 						Revolutionizing Cell Therapy Monitoring & Analysis
 					</div>
-					<div className="text-lg sm:text-xl xl:text-2xl 2xl:text-3xl flex flex-col gap-6 font-light leading-snug">
+					<div className="text-lg sm:text-xl lg:text-2xl xl:text-2xl 2xl:text-3xl flex flex-col gap-6 font-light leading-snug">
 						<div>
 							By leveraging PET reporter technology, our innovative approach
 							enables early detection of low efficacy or high toxicity
@@ -27,8 +27,8 @@ const About = () => {
 						</div>
 					</div>
 				</div>
-				<div className="col-span-2 flex items-center justify-end">
-					<div className="w-1/2 items-center grid gap-4 grid-cols-3 lg:grid-cols-1 lg:grid-rows-3 lg:col-start-6">
+				<div className="col-span-2 flex justify-center items-center lg:justify-end">
+					<div className=" lg:w-3/4 items-center grid gap-4 grid-cols-3 lg:grid-cols-1 lg:grid-rows-3 lg:col-start-6">
 						<div className=" lg:-translate-x-1/2 lg:translate-y-1/4">
 							<HexagonInfo icon={<BeakerIcon />} />
 						</div>
