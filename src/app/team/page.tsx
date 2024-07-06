a88e741c33cfce5d7b952316a69b08ef40e82308
Jake <jake.goldiee@gmail.com>
2024-07-06 11:31:22 -0400

    Add linked in link to team members

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -1,3 +1,4 @@
+import { H1, H2 } from "@/components/Header";
 import { Section } from "@/components/section";
 import { TeamMember } from "@/components/team-member";
 
@@ -5,23 +6,30 @@ const Team = () => {
 	return (
 		<Section
 			id="meet-the-team"
-			className="h-full w-full grid grid-cols-1 lg:grid-cols-3 items-center bg-[#A0C8EE] gap-24"
+			bgColor="bg-sky-50"
+			className="flex flex-col justify-center gap-12"
 		>
-			<TeamMember
-				image="/YanivHeadshot.png"
-				name="Dr. Yaniv Tivon"
-				role="Founder and Principle Investigator"
-			/>
-			<TeamMember
-				image="/GilbertHeadshot.png"
-				role="CFO"
-				name="Gilbert Brook"
-			/>
-			<TeamMember
-				image="/JasonLohmueller.png"
-				role="Advisor"
-				name="Jason Lohmueller"
-			/>
+			<H1 className="text-center lg:text-start">Meet The Team</H1>
+			<div className="grid grid-cols-1 lg:grid-cols-3 items-center gap-12">
+				<TeamMember
+					image="/YanivHeadshot.png"
+					name="Dr. Yaniv Tivon"
+					role="Founder and Principle Investigator"
+					linkedInUrl="https://www.linkedin.com/in/yanivtivon/"
+				/>
+				<TeamMember
+					image="/GilbertHeadshot.png"
+					role="CFO"
+					name="Gilbert Brook"
+					linkedInUrl="https://www.linkedin.com/in/gilbert-brook-a11353169/"
+				/>
+				<TeamMember
+					image="/JasonLohmueller.png"
+					role="Advisor"
+					name="Jason Lohmueller"
+					linkedInUrl="https://www.linkedin.com/in/jason-lohmueller-1663b551/"
+				/>
+			</div>
 		</Section>
 	);
 };
--- src/components/team-member/index.tsx
+++ src/components/team-member/index.tsx
@@ -1,33 +1,45 @@
 import { faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { motion } from "framer-motion";
 
 interface TeamMemberProps {
 	image: string;
 	role: string;
 	name: string;
+	linkedInUrl: string;
 }
 
-export const TeamMember = ({ image, role, name }: TeamMemberProps) => {
+export const TeamMember = ({
+	image,
+	role,
+	name,
+	linkedInUrl,
+}: TeamMemberProps) => {
 	return (
-		<div className="h-fit w-full max-w-[400px] flex flex-col place-self-center border border-white/40 bg-gradient-to-b from-white/40 to-white/20 backdrop-blur-sm px-6 py-12 shadow-xl rounded-lg">
-			<div className="w-full h-full flex justify-center">
+		<div className="relative h-full w-full max-w-[400px] flex flex-col place-self-center border border-white/40 bg-gradient-to-b from-white/40 to-white/20 backdrop-blur-sm px-6 py-12 shadow-xl rounded-lg">
+			<div className="flex-1 w-full h-full flex justify-center">
 				<img
 					src={image}
-					alt=""
+					alt={`${name} Headshot`}
 					className="object-cover w-full h-full rounded shadow border border-black/10"
 				/>
 			</div>
-			<div className=" flex flex-col gap-2">
+			<div className=" flex flex-col gap-2 mt-6">
 				<div>
-					<div className="w-full text-center font-semibold text-2xl">
-						{name}
+					<div className="w-fit mx-auto h-fit">
+						<div className="font-semibold text-2xl">{name}</div>
 					</div>
 				</div>
 				<div className="w-full text-center">{role}</div>
-				<div className="py-1 px-2 rounded bg-[#0072b1] w-fit h-fit">
-					<FontAwesomeIcon icon={faLinkedinIn} color="white" />
-				</div>
 			</div>
+			<a href={linkedInUrl} target="_blank" referrerPolicy="no-referrer">
+				<motion.div
+					className="py-1 px-2 rounded bg-[#0072b1] w-fit h-fit absolute bottom-2 right-2"
+					whileHover={{ filter: "brightness(1.25)" }}
+				>
+					<FontAwesomeIcon icon={faLinkedinIn} color="white" />
+				</motion.div>
+			</a>
 		</div>
 	);
 };
