c2eb6010cb0f048eb4c168a8962f99d1087e2949
Jake <jake.goldiee@gmail.com>
2024-07-06 11:04:30 -0400

    Update section sizes and allow overflow

--- src/components/section/index.tsx
+++ src/components/section/index.tsx
@@ -3,19 +3,28 @@ import cn from "@/utilities/cn";
 
 const marginTop = `my-${navbarHeight + 4}`;
 
-const SECTION_CLASSES = "h-svh w-full overflow-hidden flex justify-center";
+const SECTION_CLASSES = "min-h-screen min-w-screen flex justify-center";
 const CONTAINER_CLASSES = cn(
-	`max-w-[1920px] pt-24 pb-6 mb-4 h-full px-4 sm:px-8 md:px-12 xl:px-24`
+	`max-w-[1920px] pt-24 pb-6 px-4 sm:px-8 md:px-12 xl:px-24`
 );
 
 interface SectionProps {
 	children: React.ReactNode;
 	className?: string;
 	id: string;
+	bgColor?: string;
+	bgElement?: React.ReactNode;
 }
-export const Section = ({ children, className, id }: SectionProps) => {
+export const Section = ({
+	children,
+	className,
+	id,
+	bgColor,
+	bgElement,
+}: SectionProps) => {
 	return (
-		<section id={id} className={cn(SECTION_CLASSES)}>
+		<section id={id} className={cn(SECTION_CLASSES, bgColor)}>
+			{bgElement && bgElement}
 			<div className={cn(CONTAINER_CLASSES, className)}>{children}</div>
 		</section>
 	);
